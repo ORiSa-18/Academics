ccb2a927595fdc181a8bf4e492913711ea337fd5
Rusi1810 <126585744+Rusi1810@users.noreply.github.com>
2024-07-29 15:48:34 +0530

    Update dfs_and_bfs.py

--- 5th Sem/AI Lab/lab 1/dfs_and_bfs.py
+++ 5th Sem/AI Lab/lab 1/dfs_and_bfs.py
@@ -1,1 +1,42 @@
 
+def bfs(graph):
+    que=[]
+    for i in graph:
+        que.append(i)
+        break
+    for root in que:
+        for element in graph[root]:
+            if element not in que:
+                que.append(element)
+        print(root)
+
+def dfs(graph):
+    stack =[]
+    visited = []
+    for i in graph:
+        stack.append(i)
+        break
+    while stack:
+        current = stack.pop()
+        visited.append(current)
+        for element in graph[current]:
+            if element not in stack:
+                stack.append(element)
+    for i in visited[::-1]:
+        print(i)
+
+
+
+def main():
+    graph1 = {
+        'A': ['B', 'C'],
+        'B': ['D', 'E'],
+        'C': ['F'],
+        'D': [],
+        'E': ['F'],
+        'F': []
+    }
+    dfs(graph1)
+
+
+main()
