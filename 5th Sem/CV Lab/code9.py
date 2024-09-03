30e0b4a5abde53661248300f74cfe1970d9f03a3
Rusi1810 <126585744+Rusi1810@users.noreply.github.com>
2024-09-03 15:44:17 +1100

    Update code9.py

--- 5th Sem/CV Lab/code9.py
+++ 5th Sem/CV Lab/code9.py
@@ -19,5 +19,29 @@ rotated_image = cv2.warpAffine(img, m, (new_width, new_height))
 # Display images
 cv2.imshow("Original Image", img)
 cv2.imshow("Rotated Image", rotated_image)
+
+# shrink image
+img_shrink = cv2.resize(img,(250,250),interpolation=cv2.INTER_AREA)
+cv2.imshow('Shrink',img_shrink)
+# enlarge image
+img_enlarge = cv2.resize(img_shrink,None,fx=1.5,fy=1.5,interpolation=cv2.INTER_CUBIC)
+cv2.imshow("enlarged",img_enlarge)
+
+# translate image
+m = np.float32([[1,0,100],[0,1,50]])
+dst = cv2.warpAffine(img,m,(width,height))
+dst = cv2.resize(dst,((int)(height/4),(int)(width/4)))
+cv2.imshow("translate",dst)
+
+# Get original dimensions
+height, width = img.shape[:2]
+# shear an image
+m=np.float32([[1,0.5,0],[0,1,0],[0,0,1]])
+img_shear = cv2.warpPerspective(img,m,(int(width*1.5),int(height*1.5)))
+img_shear = cv2.resize(img_shear,((int)(height/4),(int)(width/4)))
+cv2.imshow("shear",img_shear)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
